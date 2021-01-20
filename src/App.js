c7dc4e246b64dae7d67a7cf1805be63b39a63a1c
Linlin <linlin@d-i89-175-126.student.eduroam.uq.edu.au>
2021-01-20 16:54:15 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.module.css';
 
-function App() {
-  return (
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+class App extends Component {
+  render (){
+    return (
     <div>
-      <p>Hi</p>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     </div>
-  );
+   );
+  }
 }
 
 export default App;
